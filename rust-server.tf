927cbbc5eebb41dbe32145fa17146e8a25f45c1a
Jonathan Porta <jonathan@jonathanPorta.com>
2015-09-05 22:28:40 -0700

    Comment out some broken stuffs

--- rust-server.tf
+++ rust-server.tf
@@ -20,28 +20,28 @@ resource "google_compute_instance" "default" {
 
         }
     }
-
-  metadata {
-    gce-initial-windows-user = "Administrator"
-    gce-initial-windows-password = "${var.admin_password}"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
-    ]
-    connection {
-      type = "winrm"
-      user = "Administrator"
-      password = "${var.admin_password}"
-    }
-  }
+# Provisioner and credential setting doesn't work with GCE's new Windows Auth system...
+#  metadata {
+#    gce-initial-windows-user = "Administrator"
+#    gce-initial-windows-password = "${var.admin_password}"
+#  }
+#
+#  provisioner "remote-exec" {
+#    inline = [
+#      "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
+#    ]
+#    connection {
+#      type = "winrm"
+#      user = "Administrator"
+#      password = "${var.admin_password}"
+#    }
+#  }
 }
 
 
-    #config.vm.provision 'shell', inline: ''
-    #config.vm.provision 'shell', inline: 'echo "Launching chef powershell installer script..." ; iex C:\vagrant\powershell\chef-installer.ps1'
-    #config.vm.provision 'shell', inline: 'cinst -y git.install'
-    #config.vm.provision 'shell', inline: 'echo "Launching chef repo powershell script..." ; iex C:\vagrant\powershell\chef-repo.ps1'
-    # TODO: Figure out how in the hell these dependencies get on the production server, because I am pretty sure this is not the right way...
-    #config.vm.provision 'shell', inline: 'echo "Launching chef-solo powershell script..." ; iex C:\vagrant\powershell\chef-solo.ps1'
+#config.vm.provision 'shell', inline: ''
+#config.vm.provision 'shell', inline: 'echo "Launching chef powershell installer script..." ; iex C:\vagrant\powershell\chef-installer.ps1'
+#config.vm.provision 'shell', inline: 'cinst -y git.install'
+#config.vm.provision 'shell', inline: 'echo "Launching chef repo powershell script..." ; iex C:\vagrant\powershell\chef-repo.ps1'
+# TODO: Figure out how in the hell these dependencies get on the production server, because I am pretty sure this is not the right way...
+#config.vm.provision 'shell', inline: 'echo "Launching chef-solo powershell script..." ; iex C:\vagrant\powershell\chef-solo.ps1'
