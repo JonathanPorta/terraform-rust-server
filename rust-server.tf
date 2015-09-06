609741262c8fd7105c579337fd40549642a98542
Jonathan Porta <jonathan@jonathanPorta.com>
2015-09-06 00:24:51 -0700

    Add firewall config

--- rust-server.tf
+++ rust-server.tf
@@ -6,42 +6,39 @@ provider "google" {
 }
 
 resource "google_compute_instance" "default" {
-    name = "rust"
-    machine_type = "g1-small"
-    zone = "us-central1-f"
+  name = "rust"
+  machine_type = "g1-small"
+  zone = "us-central1-f"
 
-    disk {
-        image = "windows-server-2012-r2-dc-v20150813"
+  disk {
+    image = "windows-server-2012-r2-dc-v20150813"
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = "104.197.31.88"
     }
-
-    network_interface {
-        network = "default"
-        access_config {
-
-        }
-    }
-# Provisioner and credential setting doesn't work with GCE's new Windows Auth system...
-#  metadata {
-#    gce-initial-windows-user = "Administrator"
-#    gce-initial-windows-password = "${var.admin_password}"
-#  }
-#
-#  provisioner "remote-exec" {
-#    inline = [
-#      "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
-#    ]
-#    connection {
-#      type = "winrm"
-#      user = "Administrator"
-#      password = "${var.admin_password}"
-#    }
-#  }
+  }
 }
 
+resource "google_compute_firewall" "default" {
+  name = "rust"
+  network = "default"
 
-#config.vm.provision 'shell', inline: ''
-#config.vm.provision 'shell', inline: 'echo "Launching chef powershell installer script..." ; iex C:\vagrant\powershell\chef-installer.ps1'
-#config.vm.provision 'shell', inline: 'cinst -y git.install'
-#config.vm.provision 'shell', inline: 'echo "Launching chef repo powershell script..." ; iex C:\vagrant\powershell\chef-repo.ps1'
-# TODO: Figure out how in the hell these dependencies get on the production server, because I am pretty sure this is not the right way...
-#config.vm.provision 'shell', inline: 'echo "Launching chef-solo powershell script..." ; iex C:\vagrant\powershell\chef-solo.ps1'
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports = ["80", "28055"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports = ["80", "28055"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
